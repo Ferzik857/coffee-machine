aff9dab593c28c341b56bc5cda1c7cc1c96ef75b
Ferzik857 <Aleksandar.Pastuhov@yandex.ru>
2020-04-21 18:06:10 +0300

    машина готовит кофе

--- index.php
+++ index.php
@@ -12,21 +12,21 @@
   </head>
   <body>
   <div class="container">
-    <div class="row coffee-machine">
+    <div class="row coffee-machine" id="coffee">
       <div class="col-6 coffee-list d-flex flex-column justify-content-around">
-        <div class="coffee-item">
+        <div class="coffee-item" onclick="buyCoffee('Американо', 50, this)">
           <img src="img/americano.png" alt="">
           <span>Американо - 50 руб.</span>
         </div>
-        <div class="coffee-item">
+        <div class="coffee-item" onclick="buyCoffee('капучино', 92, this)">
           <img src="img/cappuccino.png" alt="">
           <span>капучино - 92 руб.</span>
         </div>
-        <div class="coffee-item">
+        <div class="coffee-item" onclick="buyCoffee('экспрессо', 66, this)">
           <img src="img/espresso.png" alt="">
           <span>экспрессо - 66 руб.</span>
         </div>
-        <div class="coffee-item">
+        <div class="coffee-item" onclick="buyCoffee('Латте', 128, this)">
           <img src="img/latte.jpg" alt="">
           <span>Латте - 128 руб.</span>
         </div>
@@ -37,12 +37,11 @@
             <div class="display">
              <p class="display-text"> Выберите кофе</p>
               <div class="progress">
-               <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%">
-               </div>
+               <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 0%"></div>
               </div>
             </div>
             <div class="coffee-cup">
-              <img src="img/americano.png" alt="">
+              <img class="d-none" src="img/americano.png" alt="">
            </div>
           </div>
            <div class="col-6">
@@ -64,6 +63,7 @@
     </div>
   </div>
     <!-- Optional JavaScript -->
+    <script src="coffee.js"></script>
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
---
+++ script.js
@@ -0,0 +1,62 @@
+"use strict"
+//window.document.documentElement.body
+//console.log(document.body);
+//let coffeeMachine = document.getElementById("coffee"); // Поиск по Id st m
+//console.log(coffeeMachine);
+//let images = document.getElementsByTagName("img");//po teg st m
+//console.log(images);
+//let coffeeItems = document.getElementsByClassName("coffee-item");//po class st m
+//console.log(coffeeItems[0]);
+//let coffeeMachine = document.querySelector("#coffee");
+//console.log(coffeeMachine);
+//let images = document.querySelector("img");
+//console.log(images);
+//let coffeeItems = document.querySelectorAll(".coffee-item")
+//console.log(coffeeItems);
+//let itemImages = document.querySelectorAll(".coffee-item img")
+//console.log(itemImages);
+//let cupImages = document.querySelectorAll(".coffee-item img,.coffee-cup img")
+//console.log(cupImages);
+//let coffeeMachine = document.querySelector(".coffee-machine");
+//coffeeMachine.style.border = "10px solid darkblue ";
+//coffeeMachine.style.borderRadius = "25px";
+//coffeeMachine.style.position = "absolute";
+//coffeeMachine.style.top = "15px";
+//coffeeMachine.style.left = "150px";
+//let coffeeMachineTop = coffeeMachine.style.top;
+//console.log(parseInt(coffeeMachineTop));
+//let balance = document.querySelector("input[type='text']");
+//let balanceType = balance.getAttribute("type");
+//console.log(balanceType);
+//balance.setAttribute("type", "date");
+//
+//console.log(balance.hasAttribute ("placeholder"));
+//
+//balance.removeAttribute("aria-label");
+//balance.value = 500;
+//console.log(balance.value);
+
+//class
+
+//let changeButton = document.querySelector(".btn");
+//console.log(changeButton.classList);
+//changeButton.classList.remove("btn-primary");
+//changeButton.classList.add("btn-success");
+//changeButton.classList.toggle("")
+//let displayText = document.querySelector(".display-text");
+//console.log(displayText.innerHTML);
+//console.log(displayText.innerText);
+
+//displayText.innerHTML = "<b> Готовим кофе </b>"
+//displayText.innerText = "<b> Готовим кофе </b>"
+
+
+
+
+
+
+
+
+
+
+
